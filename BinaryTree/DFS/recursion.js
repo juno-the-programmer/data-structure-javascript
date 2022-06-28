2691e9d4f7791a5fe77fc0bf6450fcc40c68ed18
Juno Goh <junoinbox@gmail.com>
2022-06-28 13:34:48 +0800

    Binary tree depth first search.

---
+++ BinaryTree/DFS/recursion.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const depthFirstValues = (root) => {
+  if (root == null) return [];
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+  return [root.val, ...leftValues, ...rightValues];
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const result = depthFirstValues(a);
+console.log(result);
+/*
+Time: O(n)
+Space: O(n)
+*/
